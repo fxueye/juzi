84ffe4ed66b4a47f5d175bd9297e0aee53145903
skw <281431280@qq.com>
2017-12-25 15:46:26 +0800

    first commit

---
+++ common/src/main/java/com/zhaobaoge/common/util/AppUtil.java
@@ -0,0 +1,25 @@
+package com.zhaobaoge.common.util;
+
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+
+import java.util.List;
+
+
+public class AppUtil {
+    /**
+     *判断当前应用程序处于前台还是后台
+     */
+    public static boolean isApplicationBroughtToBackground(final Context context) {
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (!tasks.isEmpty()) {
+            ComponentName topActivity = tasks.get(0).topActivity;
+            if (!topActivity.getPackageName().equals(context.getPackageName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ common/src/main/java/com/zhaobaoge/common/util/FillUtil.java
@@ -0,0 +1,26 @@
+package com.zhaobaoge.common.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FillUtil {
+    /**
+     * 按行读取文本文件
+     *
+     * @param is
+     * @return
+     * @throws Exception
+     */
+    public static String readTextFromFile(InputStream is) throws Exception {
+        InputStreamReader reader = new InputStreamReader(is);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        StringBuffer buffer = new StringBuffer("");
+        String str;
+        while ((str = bufferedReader.readLine()) != null) {
+            buffer.append(str);
+            buffer.append("\n");
+        }
+        return buffer.toString();
+    }
+}
